5092d2b826779ccf522fb47b3ba06b262b4413f6
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 17:30:07 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
         {name: "Romeo", age: "15", status: "alive"}
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -32,5 +32,5 @@ def third_challenge
    }
   }
 
-  
+
 end
