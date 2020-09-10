e8983ef96757ed2cbda622ea7343b15ef37ccddf
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 17:33:57 +0000

    Done.

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,10 +2,31 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      }, 
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :hero => {
+        #your key/value pairs here
+        },
+      :hero_friends => []
+      },
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch => {
+        #your key/value pairs here
+        },
+      :matriarch => {
+        #your key/value pairs here
+        },
+      :heroine => {
+        #your key/value pairs here
+        },
+      :heroine_friends => []
+      }
+    }
 
-  
+
 end
